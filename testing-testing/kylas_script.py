06d737065bb515c7eb916cdf768c409fd843ad60
Kyla Cheung <kylaycheung@gmail.com>
2013-09-06 19:52:15 -0400

    added kyla.out

--- testing-testing/kylas_script.py
+++ testing-testing/kylas_script.py
@@ -1,5 +1,12 @@
+import sys
+
 def main():
-    print "welcome data team"
+    args = sys.argv[1:]
+
+    print "Welcome, Data Team!"
+
+    for arg in args:
+        print arg
 
 
 if __name__ == "__main__":
